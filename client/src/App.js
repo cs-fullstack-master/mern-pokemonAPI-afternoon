f04c1ddbaf0a3df56874e9aec84017be4114b62c
kevin-CodeCrew <kevin@code-crew.org>
2019-04-17 04:45:06 -0500

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -9,24 +9,9 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-                    <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link to="/" className="navbar-brand">Pokedex</Link>
 
-                        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                                aria-label="Toggle navigation">
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarResponsive">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="/">Home
-                                        <span className="sr-only">(current)</span>
-                                    </a>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
                 </nav>
                 <div className="grid-container grid-container--fill">
                     <Route path="/" exact component={ListPokemon}/>
--- client/src/components/view-pokemon.js
+++ client/src/components/view-pokemon.js
@@ -35,7 +35,6 @@ export default class ViewPokemon extends Component {
     }
 
 
-
     render() {
         if (this.state.toHomePage === true) {
             return <Redirect to={'/'}/>
@@ -45,27 +44,27 @@ export default class ViewPokemon extends Component {
             <li key={item.ability.url}>{item.ability.name}</li>) : [];
 
         let moves = this.state.pokemon_moves ? this.state.pokemon_moves.map((item, key) =>
-            <li key={item.move.url}>{item.move.name}</li>):[];
+            <li key={item.move.url}>{item.move.name}</li>) : [];
 
-        // let sprites= this.state.pokemon_sprites ? this.state.pokemon_sprites.map((item, key) =>
-        //      <img src={item} alt="Pokemon Image"/>):[];
-
-        let sprites = <img src={this.state.pokemon_sprites.front_default} alt="Pokemon"/>
 
         console.log(this.state.pokemon_sprites);
 
         return (
             <div className="grid-container">
-                <div className="grid-item">View Pokemon
-                <ul>Abilities:
-                    {abilities}
-                </ul>
-                <ul>Moves:
-                    {moves}
-                </ul>
+                <div className="grid-item">
+                    <ul><h1>Abilities</h1>
+                        {abilities}
+                    </ul>
+                    <ul><h1>Moves</h1>
+                        {moves}
+                    </ul>
                 </div>
                 <div className="grid-images">
-                    {sprites}
+                    {/*{sprites}*/}
+                    <img src={this.state.pokemon_sprites.front_default} alt="Pokemon"/>
+                    <img src={this.state.pokemon_sprites.back_default} alt="Pokemon"/>
+                    <img src={this.state.pokemon_sprites.front_shiny} alt="Pokemon"/>
+
                 </div>
             </div>
         )
